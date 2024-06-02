82a71a28b5d9a9a45a385100a945d8d0ea6063ed
Lucas <lucasefernandes333@gmail.com>
2024-06-01 22:55:39 -0300

    Create task endpoint and dockercompose for db

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
+mod schema;
+mod model;
 mod services;
-
+use env_logger::Env;
 use actix_web::{
+    web,
     App,
     HttpServer,
 };
 
+use dotenv::dotenv;
+use sqlx::{
+    Postgres,
+    Pool,
+    postgres::PgPoolOptions,
+};
+
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    println!("Starting server at port 8080");
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new().max_connections(10).connect(&database_url).await {
+        Ok(pool) => {
+            println!("db connection resolved");
+            pool
+        }
+        Err(e) => {
+            println!("db connection failed: {:?}", e);
+            std::process::exit(1);
+        }
+    };
     
     let _ = HttpServer::new( move || {
         App::new()
+        .app_data(web::Data::new(AppState {
+            db: pool.clone()
+        }))
         .configure(services::config)
     })
     .bind(("127.0.0.1", 8080))?
--- src/services.rs
+++ src/services.rs
@@ -1,15 +1,19 @@
 use actix_web::{
     web::{
         scope,
+        Json,
+        Data,
         ServiceConfig,
     },
     get,
+    post,
     HttpResponse,
     Responder
 };
 
 use serde_json::json;
-
+use sqlx;
+use crate::{schema::CreateTaskSchema, model::TaskModel, AppState};
 
 #[get("/healthchecker")]
 async fn healthchecker() -> impl Responder {
@@ -18,8 +22,57 @@ async fn healthchecker() -> impl Responder {
     }))
 }
 
+#[post("/test")]
+async fn test() -> impl Responder {
+    println!("test reached");
+    HttpResponse::Ok().json(json!({
+        "status": "ok"
+    }))
+}
+
+#[post("/task")]
+async fn create_task(
+    body: Json<CreateTaskSchema>,
+    data: Data<AppState> ) -> impl Responder {
+    
+    println!("task reached");
+
+    match
+        sqlx::query_as!(
+            TaskModel,
+            "INSERT INTO tasks (title, content) VALUES ($1, $2) RETURNING *",
+            body.title.to_string(),
+            body.content.to_string()
+        )
+        .fetch_one(&data.db)
+        .await {
+            Ok(task) => {
+                let note_response = json!({
+                    "status": "ok",
+                    "task": json!({ "task":task })
+                });
+
+                return HttpResponse::Ok().json(note_response);
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                let error_response = json!({
+                    "status": "error",
+                    "message": format!("{:?}", e)
+                });
+
+                return HttpResponse::InternalServerError().json(error_response);
+            }
+        }
+} 
+
+
 
 pub fn config(conf: &mut ServiceConfig) {
-    let scope = scope("/api").service(healthchecker);
+    let scope = scope("/api")
+        .service(healthchecker)
+        .service(create_task)
+        .service(test);
+
     conf.service(scope);
 }
